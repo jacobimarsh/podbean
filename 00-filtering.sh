9ab70aa196f76ea400d0bf3d40e190069d9cb3e5
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2021-10-08 12:06:52 +0800

    Update 00-filtering.sh

--- 00-filtering.sh
+++ 00-filtering.sh
@@ -8,6 +8,8 @@ vcftools --vcf ${i}_SNP_all.vcf.recode.vcf --max-missing 0.1 --maf 0.01 --minQ 3
 vcftools --vcf ${i}_indel_all.vcf.recode.vcf --max-missing 0.9 --non-ref-af-any 0.01 --recode --out ${i}_q30_m0.9_af0.01_INDEL
 mv ${i}_q30_m0.9_af0.01_INDEL.recode.vcf ${i}_indel_filt_all.vcf
 mv ${i}_q30_m0.1_af0.05_SNP.recode.vcf ${i}_SNP_filt_all.vcf
-beagle gt=${i}_SNP_filt_all.vcf nthreads=22 out=impu_${i}_SNP_filt_all
-bgzip -cd impu_${i}_SNP_filt_all.vcf.gz >> impu_${i}_SNP_filt_all.vcfzz"
+bcftools annotate --set-id +'%CHROM\_%POS' /scratch/pawsey0149/jmarsh1/podshatter/snp_calling/${i}_SNP_filt_all.vcf > /scratch/pawsey0149/jmarsh1/podshatter/snp_calling/${i}_SNP_id_filt_all.vcf
+bcftools view --max-alleles 2 /scratch/pawsey0149/jmarsh1/podshatter/snp_calling/${i}_SNP_id_filt_all.vcf > /scratch/pawsey0149/jmarsh1/podshatter/snp_calling/${i}_SNP_id_filt_bial_all.vcf
+java -Xmx100g -jar /scratch/pawsey0149/jmarsh1/podshatter/snp_calling/beagle.28Jun21.220.jar gt=/scratch/pawsey0149/jmarsh1/podshatter/snp_calling/${i}_SNP_id_filt_bial_all.vcf nthreads=15 out=/scratch/pawsey0149/jmarsh1/podshatter/snp_calling/impu_bial_${i}_SNP_id_filt_all
+bgzip -cd /scratch/pawsey0149/jmarsh1/podshatter/snp_calling/impu_bial_${i}_SNP_id_filt_all.vcf.gz > /scratch/pawsey0149/jmarsh1/podshatter/snp_calling/impu_bial_${i}_SNP_id_filt_all.vcfzz
 done | sed 's/aa/"/' | sed 's/zz/"/'
