93d697210b7a0e4c3bac20fb1353d9549a36b842
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2021-10-26 15:34:16 +0800

    Update 04-locate_pdh1.sh

--- 04-locate_pdh1.sh
+++ 04-locate_pdh1.sh
@@ -13,5 +13,5 @@ done
 #Hits for Glyma on Chr16 (2.5kb; 100%), Chr02 (1.1kb), Chr10 (150bp) and Chr14 (156bp)
 #Hits for Phavu on Chr02 (550bp) and Chr03 (550bp)
 #Hits for Phalu on Chr02 (550bp) and Chr03 (470bp)
-#Hits for Vigun on Chr03 (two; at 559bp and 470bp)
+#Hits for Vigun on Chr03 (two; 559bp and 470bp)
 #Hits for Vigan on NC_030637.1 (550bp)
