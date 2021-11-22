d9e07f8abcae159d0047f4ce3a0da0280ab98ee6
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2021-11-22 16:27:48 +0800

    Update 06-anno2phylo_input.sh

--- 06-anno2phylo_input.sh
+++ 06-anno2phylo_input.sh
@@ -1,3 +1,4 @@
+conda activate blast
 conda install -c bioconda seqkit
 conda install -c bioconda bioawk
 
@@ -6,3 +7,12 @@ awk '{print $1":"$22":"$17":"$18":"$24"-"$25":"$19":"$20"-"$21}' musclesites_inf
 sed 's/.t1//' soybean_prob.aa > AUG_pre.aa
 seqkit replace -p "(.+)" -r '{kv}' -k naming_AUG.tmp AUG_pre.aa >> a.txt
 cat a.txt | bioawk -c fastx ' $name ~ /.....:/ { print ">" $name "\n" $seq }' > soybean_prob_AUGonly_fin.aa
+
+
+grep '>' muscle_flanks.fa | sed 's/>//' > name_changing_muscleflanks.tmp
+awk '{print $1":"$22":"$17":"$18":"$2":"$7"-"$8}' musclesites_info.mtx | paste name_changing_muscleflanks.tmp - > naming_MFs.tmp
+seqkit replace -p "(.+)" -r '{kv}' -k naming_MFs.tmp muscle_flanks.fa >> muscle_flanks_renamed.aa
+grep AUGUSTUS naming_MFs.tmp > naming_AUG_MFs.tmp
+seqkit replace -p "(.+)" -r '{kv}' -k naming_AUG_MFs.tmp muscle_flanks.fa >> AUG_MFaa.tmp
+cat AUG_MFaa.tmp | bioawk -c fastx ' $name ~ /.....:/ { print ">" $name "\n" $seq }' > muscle_flanks_AUG_renamed.aa
+
