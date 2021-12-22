eee2111cf345748ee04a43fb47049293f28369cd
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2021-12-22 15:24:45 +0800

    Update 06-anno2phylo_input.sh

--- 06-anno2phylo_input.sh
+++ 06-anno2phylo_input.sh
@@ -2,10 +2,10 @@ conda activate blast
 conda install -c bioconda seqkit
 conda install -c bioconda bioawk
 
-awk '{print $30}' musclesites_info.mtx >> namechanging_AUGUSTUS.temp
+awk '{print $30}' musclesites_info.mtx > namechanging_AUGUSTUS.temp
 awk '{print $1":"$22":"$17":"$18":"$24"-"$25":"$19":"$20"-"$21}' musclesites_info.mtx | paste namechanging_AUGUSTUS.temp - > naming_AUG.tmp
 sed 's/.t1//' soybean_prob.aa > AUG_pre.aa
-seqkit replace -p "(.+)" -r '{kv}' -k naming_AUG.tmp AUG_pre.aa >> a.txt
+seqkit replace -p "(.+)" -r '{kv}' -k naming_AUG.tmp AUG_pre.aa>> a.txt
 cat a.txt | bioawk -c fastx ' $name ~ /.....:/ { print ">" $name "\n" $seq }' > soybean_prob_AUGonly_fin.aa
 
 
@@ -13,9 +13,9 @@ grep '>' muscle_flanks.fa | sed 's/>//' > name_changing_muscleflanks.tmp
 ##need a way to match lines from each before doing the next step..
 #did manually, but need to check if repeating
 awk '{print $1":"$22":"$17":"$18":"$2":"$7"-"$8":"$12}' musclesites_info.mtx | paste name_changing_muscleflanks.tmp - > naming_MFs.tmp
-seqkit replace -p "(.+)" -r '{kv}' -k naming_MFs.tmp muscle_flanks.fa >> muscle_flanks_renamed.fa
+seqkit replace -p "(.+)" -r '{kv}' -k naming_MFs.tmp muscle_flanks.fa > muscle_flanks_renamed.fa
 grep AUGUSTUS naming_MFs.tmp > naming_AUG_MFs.tmp
-seqkit replace -p "(.+)" -r '{kv}' -k naming_AUG_MFs.tmp muscle_flanks.fa >> AUG_MFaa.tmp
+seqkit replace -p "(.+)" -r '{kv}' -k naming_AUG_MFs.tmp muscle_flanks.fa > AUG_MFaa.tmp
 cat AUG_MFaa.tmp | bioawk -c fastx ' $name ~ /.....:/ { print ">" $name "\n" $seq }' > muscle_flanks_AUG_renamed.fa
 
 
