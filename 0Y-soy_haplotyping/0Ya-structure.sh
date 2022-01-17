dad6f0a62920df935b1f34c70b13a91a6eaddd8c
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-01-17 15:10:25 +0800

    Update 0Ya-structure.sh

--- 0Y-soy_haplotyping/0Ya-structure.sh
+++ 0Y-soy_haplotyping/0Ya-structure.sh
@@ -1,6 +1,6 @@
 conda create --name py2 python=2.7.14 numpy=1.13.3
+conda activate py2
 conda install scipy
-conda install -c bioconda faststructure
 
 python /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 4 --input=impu_bial_all_SNP_id_filt.vcf
 
