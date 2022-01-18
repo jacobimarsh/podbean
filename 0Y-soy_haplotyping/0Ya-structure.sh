bd7ff8ed63b70bef6290ee52faa91feb0cd0a639
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-01-18 14:20:16 +0800

    Update 0Ya-structure.sh

--- 0Y-soy_haplotyping/0Ya-structure.sh
+++ 0Y-soy_haplotyping/0Ya-structure.sh
@@ -2,7 +2,20 @@ conda create --name py2 python=2.7.14 numpy=1.13.3
 conda activate py2
 conda install scipy
 
-#plink
-python /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 4 --input=impu_bial_all_SNP_id_filt.vcf
+plink --vcf impu_bial_16_SNP_id_filt_all.vcf --allow-extra-chr --out impu_bial_16_SNP_id_filt_all.vcf
 
-#need to fix python in path...
+python2 fastStructure/structure.py -K 1 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 2 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 3 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 4 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 5 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 6 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 7 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 8 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 9 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 10 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 11 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 12 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 13 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 14 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
+python2 fastStructure/structure.py -K 15 --prior=simple --input=impu_bial_16_SNP_id_filt_all.vcf --output=test/testoutput_simple
