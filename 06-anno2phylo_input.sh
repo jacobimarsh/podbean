89d4e75954f772561a9c36a5b6355b31b8da62ae
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-01-19 12:07:04 +0800

    Update 06-anno2phylo_input.sh

--- 06-anno2phylo_input.sh
+++ 06-anno2phylo_input.sh
@@ -7,7 +7,7 @@ awk '{print $1":"$22":"$17":"$18":"$24"-"$25":"$19":"$20"-"$21}' musclesites_inf
 sed 's/.t1//' soybean_prob.aa > AUG_pre.aa
 seqkit replace -p "(.+)" -r '{kv}' -k naming_AUG.tmp AUG_pre.aa > a.txt
 cat a.txt | bioawk -c fastx ' $name ~ /.....:/ { print ">" $name "\n" $seq }' > soybean_prob_AUGonly_fin.aa
-
+# soybean_prob_AUGonly_fin.aa represents the AUGUSTUS predicted proteins from muscle_flanks.fa
 
 grep '>' muscle_flanks.fa | sed 's/>//' > name_changing_muscleflanks.tmp
 ##need a way to match lines from each before doing the next step..
@@ -17,5 +17,4 @@ seqkit replace -p "(.+)" -r '{kv}' -k naming_MFs.tmp muscle_flanks.fa > muscle_f
 grep AUGUSTUS naming_MFs.tmp > naming_AUG_MFs.tmp
 seqkit replace -p "(.+)" -r '{kv}' -k naming_AUG_MFs.tmp muscle_flanks.fa > AUG_MFaa.tmp
 cat AUG_MFaa.tmp | bioawk -c fastx ' $name ~ /.....:/ { print ">" $name "\n" $seq }' > muscle_flanks_AUG_renamed.fa
-
-
+#muscle_flanks_renamed.fa is muscle_flanks.fa for all, AUG is for only those with an AUG hit
