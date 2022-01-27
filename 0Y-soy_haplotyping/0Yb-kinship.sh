923d8fcffcdf9f2f6a484973ce88a66432e3d17f
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-01-27 18:56:27 +0800

    Create 0Yb-kinship.sh

---
+++ 0Y-soy_haplotyping/0Yb-kinship.sh
@@ -0,0 +1,8 @@
+module load java
+tassel5 -Xmx100g -h nohet_fin_impu_bial_16_SNP_id_filt_all.hmp.txt -KinshipPlugin -method Centered_IBS -endPlugin -export nohet_fin_impu_bial_16_SNP_id_filt_all_kinship.txt -exportType SqrMatrix
+touch temp_empty.txt
+tail -n +4 nohet_fin_impu_bial_16_SNP_id_filt_all_kinship.txt | awk '{print $1}' | tr "\n" "\t" >> temp_head.txt
+paste temp_empty.txt temp_head.txt >> nohet_fin_impu_bial_16_SNP_id_filt_all_kinship_HMformatted.txt
+tail -n +4 nohet_fin_impu_bial_16_SNP_id_filt_all_kinship.txt >> nohet_fin_impu_bial_16_SNP_id_filt_all_kinship_HMformatted.txt
+rm temp_empty.txt
+rm temp_head.txt
