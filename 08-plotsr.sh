1584d1ddd2e77d903b5573a3eb875616fe530b21
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-02-24 14:24:46 +0800

    Update 08-plotsr.sh

--- 08-plotsr.sh
+++ 08-plotsr.sh
@@ -1,5 +1,7 @@
-conda activate py2
-conda install -c bioconda/label/cf201901 minimap2
+conda create --name plotsr
+conda activate plotsr
+conda install -c bioconda plotsr
+conda install -c bioconda 'minimap2>=2.7'
 minimap2 -ax asm5 -t 4 -eqx glyma1.fa glyso2.fa | samtools sort -O BAM - > glyma1_glyso2.bam
 samtools index glyma1_glyso2.bam
 minimap2 -ax asm5 -t 4 -eqx glyso2.fa glyma1.fa | samtools sort -O BAM - > glyso2_glyma1.bam
