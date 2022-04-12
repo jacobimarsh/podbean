0100c6ebb1638a301b697fb37ebb351e603a358c
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-04-12 12:07:11 +0800

    Update 0Yb-vcf2hmp.sh

--- 0Y-soy_haplotyping/0Yb-vcf2hmp.sh
+++ 0Y-soy_haplotyping/0Yb-vcf2hmp.sh
@@ -13,3 +13,12 @@ grep -v $'\tAC\t\|\tAG\t\|\tAT\t\|\tCA\t\|\tCG\t\|\tCT\t\|\tGA\t\|\tGC\t\|\tGT\t
 plink2 --vcf ../impu_bial_16_SNP_id_filt_all.vcf  --indep-pairwise 500 1 0.9 --make-bed --out indep500_1_0.9_impu_bial_16_SNP_id_filt_all
 plink --bfile indep500_1_0.9_impu_bial_16_SNP_id_filt_all --extract indep500_1_0.9_impu_bial_16_SNP_id_filt_all.prune.in --make-bed
 sed 's/0_//g' !$ > indep500_1_0.9_impu_bial_16_SNP_id_filt_all.vcf
+sed 's/0\/1/.\/./g' indep500_1_0.9_impu_bial_16_SNP_id_filt_all.vcf >> indep500_1_0.9_nohet_impu_bial_16_SNP_id_filt_all.vcf
+sed -i 's/1\/0/.\/./g' indep500_1_0.9_nohet_impu_bial_16_SNP_id_filt_all.vcf
+module load java
+java -jar /group/pawsey0149/jmarsh1/packages/beagle.18May20.d20.jar gt=indep500_1_0.9_nohet_impu_bial_16_SNP_id_filt_all.vcf nthreads=22 out=indep500_1_0.9_nohet_prefin_impu_bial_16_SNP_id_filt_all
+bgzip -d indep500_1_0.9_nohet_prefin_impu_bial_16_SNP_id_filt_all.vcf.gz
+tassel5 -Xmx20G -vcf indep500_1_0.9_nohet_prefin_impu_bial_16_SNP_id_filt_all.vcf -export indep500_1_0.9_nohet_prefin_impu_bial_16_SNP_id_filt_all -exportType HapmapDiploid
+sed -i 's/#//g' indep500_1_0.9_nohet_prefin_impu_bial_16_SNP_id_filt_all.hmp.txt
+sed -i "s/^/Gm/" indep500_1_0.9_nohet_prefin_impu_bial_16_SNP_id_filt_all.hmp.txt
+sed -i "s/Gmrs/rs/" indep500_1_0.9_nohet_prefin_impu_bial_16_SNP_id_filt_all.hmp.txt
