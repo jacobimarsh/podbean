c1cabf7e563ec0aeaa40d1d682ee2bbacaefeb6f
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-04-12 11:31:30 +0800

    Update 0Yb-vcf2hmp.sh

--- 0Y-soy_haplotyping/0Yb-vcf2hmp.sh
+++ 0Y-soy_haplotyping/0Yb-vcf2hmp.sh
@@ -9,3 +9,7 @@ sed -i "s/^/Gm/" nohet_prefin_impu_bial_16_SNP_id_filt_all.hmp.txt
 sed -i "s/Gmrs/rs/" nohet_prefin_impu_bial_16_SNP_id_filt_all.hmp.txt
 #used nohet prefin because it cut too many hets ^^
 grep -v $'\tAC\t\|\tAG\t\|\tAT\t\|\tCA\t\|\tCG\t\|\tCT\t\|\tGA\t\|\tGC\t\|\tGT\t\|\tTA\t\|\tTC\t\|\tTG\t\|\tA\t20\|\tC\t20\|\tG\t20\|\tT\t20' nohet_prefin_impu_bial_16_SNP_id_filt_all.hmp.txt >> nohet_fin_impu_bial_16_SNP_id_filt_all.hmp.txt
+#pruning for independance so it can run on 100kb region
+plink2 --vcf ../impu_bial_16_SNP_id_filt_all.vcf  --indep-pairwise 500 1 0.9 --make-bed --out indep500_1_0.9_impu_bial_16_SNP_id_filt_all
+plink --bfile indep500_1_0.9_impu_bial_16_SNP_id_filt_all --extract indep500_1_0.9_impu_bial_16_SNP_id_filt_all.prune.in --make-bed
+sed 's/0_//g' !$ > indep500_1_0.9_impu_bial_16_SNP_id_filt_all.vcf
