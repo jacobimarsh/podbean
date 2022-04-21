e94fab39e199618aa2e42541b86ae3cf8ea132d3
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-04-21 13:16:23 +0800

    Update 0Za-clustree.sh

--- 0Z-Hapviz/0Za-clustree.sh
+++ 0Z-Hapviz/0Za-clustree.sh
@@ -15,14 +15,13 @@ for i in {1..9}; do grep -A 10000 'Haplotypes$Assignment' HMGm16_301_302_indep_C
 
 run_CT_clustree.sh -prefix HMGm16_301_302_indep_MIT0.8 -thresholds 5 9 1 -pheno pheno.txt
 
-R
-library(clustree)
+echo -e "library(clustree)
 library(tidyverse)
 
-id_haps <- read.csv("C:/Users/21485753/Desktop/pod/clustreebox/HMGm16_301_302_indep_ALLCT_MIT0.8.haps", sep = "\t")
-wide_IH <- spread(id_haps, "CT", "Hap") %>% as_tibble()
+id_haps <- read.csv('C:/Users/21485753/Desktop/pod/clustreebox/HMGm16_301_302_indep_ALLCT_MIT0.8.haps', sep = '\t')
+wide_IH <- spread(id_haps, 'CT', 'Hap') %>% as_tibble()
 #nona_WIH <- wide_IH %>% replace(is.na(.), 'Z')
-pheno <- read.csv("C:/Users/21485753/Desktop/pod/clustreebox/late_shatter.txt", sep = "\t")
+pheno <- read.csv('C:/Users/21485753/Desktop/pod/clustreebox/late_shatter.txt', sep = '\t')
 
 ph_NWIH <- left_join(wide_IH,
                      pheno,
@@ -32,8 +31,8 @@ mean_na.rm <- function(x){mean(x,na.rm=T)}
 
 
 phencol <- clustree(ph_NWIH, 
-                    prefix = "CT", 
-                    node_colour = "Pheno", 
+                    prefix = 'CT', 
+                    node_colour = 'Pheno', 
                     node_colour_aggr = 'mean_na.rm', 
                     edge_width = 1, 
                     node_alpha = 0.9
@@ -43,9 +42,20 @@ phencol <- clustree(ph_NWIH,
                         oob = scales::squish, 
                         begin = 0.25, 
                         direction = -1, 
-                        name = "Pheno") +
-  scale_edge_color_continuous(high = "black", 
-                              low = "grey80") +
-  labs(edge_colour = "nIndividuals", 
-       size = "nIndividuals") +
+                        name = 'Pheno') +
+  scale_edge_color_continuous(high = 'black', 
+                              low = 'grey80') +
+  labs(edge_colour = 'nIndividuals', 
+       size = 'nIndividuals') +
   guides(edge_alpha = FALSE)
+  
+
+  
+ggsave('clustree_${1}_${2}_${3}.pdf',
+       phencol, 
+       device = 'pdf', 
+       dpi = 300,
+       height = 9,
+       width = 16,
+       units = 'in')" > clustree_${1}_${2}_${3}.R
+Rscript clustree_${1}_${2}_${3}.R
