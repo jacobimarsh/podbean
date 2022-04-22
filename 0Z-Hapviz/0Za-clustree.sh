529c8ce1054e35fefb0aa7b89c626ea6fe4f11cd
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-04-22 15:18:32 +0800

    Update 0Za-clustree.sh

--- 0Z-Hapviz/0Za-clustree.sh
+++ 0Z-Hapviz/0Za-clustree.sh
@@ -1,61 +1,96 @@
 #with late shatter scores
-nohet_fin_impu_bial_16_SNP_id_filt_all.hmp.txt
-nohet_prefin_impu_bial_16_SNP_id_filt_all.hmp.txt
+##nohet_fin_impu_bial_16_SNP_id_filt_all.hmp.txt
+##nohet_prefin_impu_bial_16_SNP_id_filt_all.hmp.txt
 #undefined groups PI366122
 #undefined groups USB.055
 #undefined groups USB.350
 #undefined groups USB027
 
+#mv HMGm16_301_302_indep_CT0.9_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.9.o
+#mv HMGm16_301_302_indep_CT0.8_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.8.o
+#mv HMGm16_301_302_indep_CT0.7_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.7.o
+#mv HMGm16_301_302_indep_CT0.6_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.6.o
+#mv HMGm16_301_302_indep_CT0.5_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.5.o
 
 
-for i in {1..9}; do grep -A 10000 'Haplotypes$Assignment' HMGm16_301_302_indep_CT0.${i}_MIT0.8.o | tail -n +3 | awk '{print $2,$3}' | head -n -2 | sed 's/ /\t/' > HMGm16_301_302_indep_CT0.${i}_MIT0.8.haps; done 
-#for i in {1..9}; do sed "s/$/\tCT0.${i}/" HMGm16_301_302_indep_CT0.${i}_MIT0.8.haps >> HMGm16_301_302_indep_ALLCT_MIT0.8.haps; done
+mv HMGm16_301_302_indep_CT0.9_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.9.o
+mv HMGm16_301_302_indep_CT0.8_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.8.o
+mv HMGm16_301_302_indep_CT0.7_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.7.o
+mv HMGm16_301_302_indep_CT0.6_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.6.o
+mv HMGm16_301_302_indep_CT0.5_MIT0.8.o HMGm16_301_302_indep_MIT0.8_CT0.5.o
 
-########
+mv HMGm16_301_302_C0.6_CT0.6_MIT0.8.o	HMGm16_301_302_MIT0.8_CT0.6.o
+mv HMGm16_301_302_C0.5_CT0.5_MIT0.9.o	HMGm16_301_302_MIT0.9_CT0.5.o
+mv HMGm16_301_302_C0.5_CT0.5_MIT0.8.o	HMGm16_301_302_MIT0.8_CT0.5.o
+mv HMGm16_301_302_C0.8_CT0.8_MIT0.8.o	HMGm16_301_302_MIT0.8_CT0.8.o
+mv HMGm16_301_302_C0.7_CT0.7_MIT0.9.o	HMGm16_301_302_MIT0.9_CT0.7.o
+mv HMGm16_301_302_C0.7_CT0.7_MIT0.8.o	HMGm16_301_302_MIT0.8_CT0.7.o
+mv HMGm16_301_302_C0.6_CT0.9_MIT0.8.o	HMGm16_301_302_MIT0.8_CT0.9.o
+mv HMGm16_301_302_C0.6_CT0.6_MIT0.9.o	HMGm16_301_302_MIT0.9_CT0.6.o
+mv HMGm16_301_302_indep_CT0.7_MIT0.9.o	HMGm16_301_302_indep_MIT0.9_CT0.7.o
+mv HMGm16_301_302_indep_CT0.7_MIT0.8.o	HMGm16_301_302_indep_MIT0.8_CT0.7.o
+mv HMGm16_301_302_indep_CT0.6_MIT0.9.o	HMGm16_301_302_indep_MIT0.9_CT0.6.o
+mv HMGm16_301_302_indep_CT0.6_MIT0.8.o	HMGm16_301_302_indep_MIT0.8_CT0.6.o
+mv HMGm16_301_302_indep_CT0.5_MIT0.9.o	HMGm16_301_302_indep_MIT0.9_CT0.5.o
+mv HMGm16_301_302_indep_CT0.5_MIT0.8.o	HMGm16_301_302_indep_MIT0.8_CT0.5.o
+mv HMGm16_301_302_C0.9_CT0.9_MIT0.9.o	HMGm16_301_302_MIT0.9_CT0.9.o
+mv HMGm16_301_302_C0.9_CT0.9_MIT0.8.o	HMGm16_301_302_MIT0.8_CT0.9.o
+mv HMGm16_301_302_C0.8_CT0.8_MIT0.9.o	HMGm16_301_302_MIT0.9_CT0.8.o
+mv HMGm16_301_302_indep_CT0.9_MIT0.9.o	HMGm16_301_302_indep_MIT0.9_CT0.9.o
+mv HMGm16_301_302_indep_CT0.9_MIT0.8.o	HMGm16_301_302_indep_MIT0.8_CT0.9.o
+mv HMGm16_301_302_indep_CT0.8_MIT0.9.o	HMGm16_301_302_indep_MIT0.9_CT0.8.o
+mv HMGm16_301_302_indep_CT0.8_MIT0.8.o	HMGm16_301_302_indep_MIT0.8_CT0.8.o
 
-run_CT_clustree.sh -prefix HMGm16_301_302_indep_MIT0.8 -thresholds 5 9 1 -pheno pheno.txt
+run_CT_clustree.sh HMGm16_301_302_indep_MIT0.8 late_shatter.txt
+run_CT_clustree.sh HMGm16_301_302_MIT0.9 late_shatter.txt
+run_CT_clustree.sh HMGm16_301_302_MIT0.8 late_shatter.txt
+
+#!/bin/bash
+PREFIX=${1?Error: no prefix provided}
+PHENO=${2?Error: no pheno provided}
+
+echo -e "ID\tHap\tCT" > ${1}_allCT.haps
+for i in {5..9}
+do
+grep -A 10000 'Haplotypes$Assignment' ${1}_CT0.${i}.o | tail -n +3 | awk '{print $2,$3}' | head -n -2 | sed 's/ /\t/' > ${1}_CT0.${i}.haps
+sed "s/$/\tCT0.${i}/" ${1}_CT0.${i}.haps >> ${1}_allCT.haps
+done
 
 echo -e "library(clustree)
 library(tidyverse)
-
-id_haps <- read.csv('C:/Users/21485753/Desktop/pod/clustreebox/HMGm16_301_302_indep_ALLCT_MIT0.8.haps', sep = '\t')
+id_haps <- read.csv('${1}_allCT.haps', sep = '\\t', stringsAsFactors = FALSE)
 wide_IH <- spread(id_haps, 'CT', 'Hap') %>% as_tibble()
 #nona_WIH <- wide_IH %>% replace(is.na(.), 'Z')
-pheno <- read.csv('C:/Users/21485753/Desktop/pod/clustreebox/late_shatter.txt', sep = '\t')
-
+pheno <- read.csv('${2}', sep = '\\t')
 ph_NWIH <- left_join(wide_IH,
                      pheno,
-                     by="ID")
-
+                     by='ID')
 mean_na.rm <- function(x){mean(x,na.rm=T)}
-
-
-phencol <- clustree(ph_NWIH, 
-                    prefix = 'CT', 
-                    node_colour = 'Pheno', 
-                    node_colour_aggr = 'mean_na.rm', 
-                    edge_width = 1, 
+phencol <- clustree(ph_NWIH,
+                    prefix = 'CT',
+                    node_colour = 'Pheno',
+                    node_colour_aggr = 'mean_na.rm',
+                    edge_width = 1,
                     node_alpha = 0.9
-                    ) + 
-  scale_colour_viridis_c(limits=c(max(top_frac(ph_NWIH,-0.1,Pheno)$Pheno),
-                                 min(top_frac(ph_NWIH,0.1,Pheno)$Pheno)),
-                        oob = scales::squish, 
-                        begin = 0.25, 
-                        direction = -1, 
+                    ) +
+  scale_colour_viridis_c(limits=c(max(top_frac(ph_NWIH,-0.1,Pheno)\$Pheno),
+                                 min(top_frac(ph_NWIH,0.1,Pheno)\$Pheno)),
+                        oob = scales::squish,
+                        begin = 0.25,
+                        direction = -1,
                         name = 'Pheno') +
-  scale_edge_color_continuous(high = 'black', 
+  scale_edge_color_continuous(high = 'black',
                               low = 'grey80') +
-  labs(edge_colour = 'nIndividuals', 
+  labs(edge_colour = 'nIndividuals',
        size = 'nIndividuals') +
   guides(edge_alpha = FALSE)
-  
 
-  
-ggsave('clustree_${1}_${2}_${3}.pdf',
-       phencol, 
-       device = 'pdf', 
+
+ggsave('clustree_${2}_${1}.pdf',
+       phencol,
+       device = 'pdf',
        dpi = 300,
        height = 9,
        width = 16,
-       units = 'in')" > clustree_${1}_${2}_${3}.R
-Rscript clustree_${1}_${2}_${3}.R
+       units = 'in')" > clustree_${2}_${1}.R
+Rscript clustree_${2}_${1}.R
