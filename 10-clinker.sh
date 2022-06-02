2e8634135a2e1c0ae2965c40a4c31cab6d628315
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-06-02 12:14:24 +0800

    Create 10-clinker.sh

---
+++ 10-clinker.sh
@@ -0,0 +1,12 @@
+##get all gffs just the region of interest, then trim down so its only region of interest
+#gff to genbank
+#Get just chromosome of interest
+
+samtools faidx ../muscle_flanks/phavu.fna
+samtools faidx ../muscle_flanks/phavu.fna phavu.UI111.gnm1.Chr03 > Chr03.fn
+
+conda activate py3
+python3 gff_to_genbank.py glyma_region.gff3 Gm16.fna
+
+#run clinker
+clinker -f *.genbank
