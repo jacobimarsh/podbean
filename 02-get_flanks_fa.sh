355c53b65b7689d99e0db1d0197fb42e481cb4df
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-06-28 11:36:55 +0800

    Create 02-get_flanks_fa.sh

---
+++ 02-get_flanks_fa.sh
@@ -0,0 +1,9 @@
+##set up tab delimited text file with overview of hits (hi_conf_blasthits.mtx)
+echo '#!/bin/bash' > get_flanks_fa.sh
+cat ../fasta_blasta2/species.txt | sed 's/$/.fna/' | sed 's/^/samtools faidx /' >> get_flanks_fa.sh
+grep norm hi_conf_blasthits.mtx | awk '{print $1, $2, $15, $16}' | sed 's/ /:/g' | sed -r 's/:([^:]*)$/-\1/' | sed 's/:/.fna /' | sed 's/^/samtools faidx /' | sed 's/$/ \>> muscle_flanks.fa/' >> get_flanks_fa.sh
+grep inv hi_conf_blasthits.mtx | awk '{print $1, $2, $16, $15}' | sed 's/ /:/g' | sed -r 's/:([^:]*)$/-\1/' | sed 's/:/.fna /' | sed 's/^/samtools faidx /' | sed 's/$/ \>> muscle_flanks.fa/' >> get_flanks_fa.sh
+chmod u+x get_flanks_fa.sh
+get_flanks_fa.sh
+
+muscle -in muscle_flanks.fa > muscle_flanks_out.mas
