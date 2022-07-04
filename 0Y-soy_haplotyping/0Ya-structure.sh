10af456bbeb91cc819c9deda0dc1a63baecaadf8
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2022-07-04 10:53:11 +0800

    Update 0Ya-structure.sh

--- 0Y-soy_haplotyping/0Ya-structure.sh
+++ 0Y-soy_haplotyping/0Ya-structure.sh
@@ -6,24 +6,24 @@ plink --vcf impu_bial_16_SNP_id_filt_all.vcf --allow-extra-chr --out impu_bial_1
 
 /group/pawsey0149/jmarsh1/packages/minconda3/bin/conda activate py2
 
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 1 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 2 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 3 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 4 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 5 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 6 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 7 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 8 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 9 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 10 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 11 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 12 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 13 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 14 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
-python2 /group/pawsey0149/jmarsh1/packages/fastStructure/structure.py -K 15 --prior=simple --input=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=/scratch/pawsey0149/jmarsh1/podshatter/glyma/haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 1 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 2 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 3 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 4 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 5 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 6 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 7 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 8 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 9 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 10 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 11 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 12 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 13 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 14 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
+python2 fastStructure/structure.py -K 15 --prior=simple --input=haploupe/impu_bial_16_SNP_id_filt_all.vcf --output=haploupe/test/testoutput_simple
 
 for i in {1..15}
-do python2 /group/pawsey0149/jmarsh1/packages/fastStructure/distructnew.py -K ${i} --input=testoutput_simple --output=test${i}_distruct.svg 
+do python2 fastStructure/distructnew.py -K ${i} --input=testoutput_simple --output=test${i}_distruct.svg 
 done
 
 #REFORMAT FOR K=10
